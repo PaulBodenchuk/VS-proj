ce0ad74ba8dac92a655dfa62e94ea554268c94c9
Pavel Bodenchuk <pavel.bodenchuk@tobii.com>
2019-09-09 15:40:26 +0300

    To add 2

--- NSubstitute1/AsyncTry/TestClass.cs
+++ NSubstitute1/AsyncTry/TestClass.cs
@@ -31,6 +31,14 @@ namespace AsyncTry
         public void Method_8()
         {
             Console.WriteLine("1");
+            Console.WriteLine("2");
+            Console.WriteLine("3.1");
+            Console.WriteLine("3.2");
+            Console.WriteLine("4");
+            Console.WriteLine("5");
+            Console.WriteLine("6");
+            Console.WriteLine("7");
+            Console.WriteLine("8");
         }
     }
 }
